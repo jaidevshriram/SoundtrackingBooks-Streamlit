1f56ce108122040bba0921038c00c7eb0f3156f9
Jaidev Shriram <jaidevtrumpet@gmail.com>
2021-11-03 06:37:03 +0530

    Added generated music

--- dashboard.py
+++ dashboard.py
@@ -2,9 +2,12 @@ import streamlit as st
 import numpy as np
 import json
 import time
+import glob
 import pandas as pd
 
 df = pd.read_csv("data/16_emotion_all.csv")
+song_names = list(map(lambda f: int(f.split('/')[-1].split('.')[0]), glob.glob("generated/*.wav")))
+print(song_names)
 
 st.title("Chapter 16: Through The Trapdoor")
 st.subheader("Paragraph/Valence/Ekman/Audio")
@@ -33,8 +36,9 @@ for i, row in df.iterrows():
         cols[1].info("Neutral")
     # cols[2].write(ekman)
 
-    if i == 1:
-        audio_file = open("data/new/0.wav", "rb")
+    if i in song_names:
+        print(i)
+        audio_file = open(f"generated/{i}.wav", "rb")
         bytes = audio_file.read()
         cols[2].audio(bytes, format='audio/wav')
 
